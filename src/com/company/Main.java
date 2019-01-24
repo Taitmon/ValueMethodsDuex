8d967a2f5d3f322d09e787f586490ae61cca1765
taitmon <taitmonlynch@gmail.com>
2019-01-24 14:53:51 -0600

    check in

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main
 
     public static void main(String[] args)
     {
-        int sum = getTotal (10,20);
+        int sum = getTotal(10, 20);
         System.out.println(sum);
 
         sum = getTotal(5, 4);
@@ -13,11 +13,30 @@ public class Main
 
         sum = getTotal(12, 12);
         System.out.println(sum);
+
+        int max = max(50, 5);
+        System.out.println(max);
     }
 
-    private  static  int getTotal(int firstNumber, int secondNumber)
+    private static int getTotal(int firstNumber, int secondNumber)
     {
         int sum = firstNumber + secondNumber;
         return sum;
+    }
+
+    private static int max(int a, int b)
+    {
+        int max = 0;
+
+        if (a > b)
+        {
+            max = a;
+        }
+        else if ( a < b)
+        {
+            max = b;
+        }
+
+        return max;
     }
 }
